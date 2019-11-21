d0bdb68ef836f49cadc65ed98e98ddc2e623493b
1301mohit <mohitkr1301@gmail.com>
2019-11-21 14:52:44 +0530

    Add function to check regex of mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,61 @@
 #!/bin/bash -x
 shopt -s extglob
-echo "Enter word"
-read word
-pat1="^[A-Z]{1}[a-z]{2,}$"
-pat2="^[A-Z]{1}[a-z]{2,}$"
-pat3="^[a-zA-Z][0-9a-zA-Z._+-]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
-if [[ $word =~ $pat3 ]];
-then
-        echo yes;
-else
-        echo no;
-fi
+
+function checkFirstName()
+{
+	echo "^[A-Z]{1}[a-z]{2,}$"
+} 
+
+function checkLastName()
+{
+	echo "^[A-Z]{1}[a-z]{2,}$"
+}
+
+function checkEmail()
+{
+	echo "^[a-zA-Z][0-9a-zA-Z._+-]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
+}
+
+function checkMobileNumber()
+{
+	echo "^[0-9]{1,}[ ][0-9]{10}$"
+}
+
+function main()
+{
+	echo "Enter First Name"
+	read firstName
+	if [[ $firstName =~ $( checkFirstName ) ]]
+	then
+		echo format of firstName is correct
+	else
+		echo format of firstName is not correct
+	fi
+	echo "Enter Last Name"
+	read lastName
+	if [[ $lastName =~ $( checkLastName ) ]]
+	then
+		echo format of lastName is correct
+	else
+		echo format of lastName is not correct
+	fi
+	echo "Enter email"
+	read email
+	if [[ $email =~ $( checkEmail ) ]]
+	then
+		echo format of email is correct
+	else
+		echo format of email is not correct
+	fi
+	echo "Enter mobile number"
+	read mobileNumber
+	if [[ $mobileNumber =~ $( checkMobileNumber ) ]]
+	then
+		echo format of mobile number is correct
+	else
+		echo format of mobile number is not correct
+	fi
+}
+
+main
+
