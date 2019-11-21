eab17cc481d9bca58b2edb1b1e8b3607f36ea221
1301mohit <mohitkr1301@gmail.com>
2019-11-21 10:46:31 +0530

    Add regex to check validity of last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,7 +3,8 @@ shopt -s extglob
 echo "Enter word"
 read word
 pat1="^[A-Z]{1}[a-z]{2,}$"
-if [[ $word =~ $pat1 ]];
+pat2="^[A-Z]{1}[a-z]{2,}$"
+if [[ $word =~ $pat2 ]];
 then
         echo yes;
 else
