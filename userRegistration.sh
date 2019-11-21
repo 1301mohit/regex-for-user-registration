3a3447fd25b84be19a918c4df81345096ab992d8
1301mohit <mohitkr1301@gmail.com>
2019-11-21 11:29:29 +0530

    Add regex pattern to check validity of email

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,7 +4,8 @@ echo "Enter word"
 read word
 pat1="^[A-Z]{1}[a-z]{2,}$"
 pat2="^[A-Z]{1}[a-z]{2,}$"
-if [[ $word =~ $pat2 ]];
+pat3="^[a-zA-Z][0-9a-zA-Z._+-]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
+if [[ $word =~ $pat3 ]];
 then
         echo yes;
 else
